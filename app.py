16698bcb1a9326d307ffd00c8f2ba94d0c42b8db
Sarvesh111999 <69581410+Sarvesh111999@users.noreply.github.com>
2026-09-17 11:06:08 +0530

    Refactor add_numbers function and update route

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def add_numbers(a, b):
+    return a + b
+
 @app.route("/")
 def home():
     return "Enerlytik CI/CD app is running"
 
 @app.route("/add/<int:a>/<int:b>")
-def add_numbers(a, b):
-    return {"result": a + b}
+def add_route(a, b):
+    return {"result": add_numbers(a, b)}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
